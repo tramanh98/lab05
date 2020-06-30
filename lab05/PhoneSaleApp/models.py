50e60a501140231e2dd7cb0a6c7c9801067dffa6
tramanhk11uit <tramanh1998tls@gmail.com>
2020-06-30 15:05:18 +0700

    update model

--- lab05/PhoneSaleApp/models.py
+++ lab05/PhoneSaleApp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+#model San Pham 
+# id
+# id Brand = Foreign Key of Hang
+# name
+# description
+# price
+#numberRemain
+#createdAt
+#updatedAt
+#avatar
+
+
+#Model Hang
+# idBrand
+#nameBrand
+#descriptionBrand
